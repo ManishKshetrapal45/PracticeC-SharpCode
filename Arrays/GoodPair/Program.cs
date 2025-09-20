8876e258232c5148ff27a018b226f1d6863b68aa
ManishKshetrapal45 <manishkshetrapal105@gmail.com>
2025-09-20 17:28:10 +0530

    Code for problem Good pair

---
+++ Arrays/GoodPair/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SeparateOddEven
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Enter length of an array");
+      int len = int.Parse(Console.ReadLine());
+      int[] A = new int[len];
+      Console.WriteLine("Enter array elements");
+      for (int j = 0; j < len; j++)
+      {
+        A[j] = int.Parse(Console.ReadLine());
+      }
+      Console.WriteLine("Enter Valuse of B");
+      var B = int.Parse(Console.ReadLine());
+      Console.WriteLine(FindGoodPair(A, B));
+
+    }
+
+    public static int FindGoodPair(int[] A, int B)
+    {
+      for (int i = 0; i < A.Length; i++)
+      {
+        for (int j = 0; j < A.Length; j++)
+        {
+          if (i == j) continue;
+          if (A[i] + A[j] == B) return 1;
+        }
+      }
+      return 0;
+    }
+  }
+}
